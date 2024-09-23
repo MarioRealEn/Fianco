Black AI now works, but I need to check the move calculation, just had a bug.

game.py
@@ -31,6 +31,17 @@ class FiancoGame:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Fianco Game')
         self.clock = pygame.time.Clock()
+        # self.initial_board_state = np.array([
+        #     [1, 0, 0, 0, 0, 0, 1, 1, 0],
+        #     [1, 1, 1, 1, 1, 1, 0, 1, -1],
+        #     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        #     [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        #     [0, 0, 0, 0, -1, 0, 0, 0, 0],
+        #     [0, 0, 0, 0, -1, 0, 0, 0, 0],
+        #     [0, 0, 0, -1, 0, 0, 0, 0, 0],
+        #     [1, -1, 0, 0, 0, 0, 0, -1, 0],
+        #     [0, -1, -1, 0, 0, -1, -1, -1, -1]
+        # ], dtype=np.int8)
         self.initial_board_state = np.array([
             [ 1,  1,  1,  1,  1,  1,  1,  1,  1],
             [ 0,  1,  0,  0,  0,  0,  0,  1,  0],
@@ -57,7 +68,7 @@ class FiancoGame:
         # Player types: 'human' or 'ai'
         self.player_types = {
             -1: 'human',  # White player
-            1: 'ai'    # Black player
+            1: 'human'    # Black player
         }
         # To set AI players, change 'human' to 'ai' for the desired player
         # Example: self.player_types[1] = 'ai'  # Black player is controlled by AI

test_env.py
@@ -1,5 +1,5 @@
 import numpy as np
-from fianco_brain import get_best_move, evaluate_board_python  # Import the Rust AI function
+from fianco_brain import get_best_move, evaluate_board_python, get_valid_moves_python  # Import the Rust AI function
 
 # board = np.array([
 #             [1, 0, 0, 0, 0, 0, 1, 1, 1],
@@ -46,10 +46,10 @@ board = np.array([
             [0, 0, 0, 0, -1, 0, 0, 0, 0],
             [0, 0, 0, 0, -1, 0, 0, 0, 0],
             [0, 0, 0, -1, 0, 0, 0, 0, 0],
-            [0, -1, 0, 0, 0, 0, 0, -1, 0],
-            [1, -1, -1, 0, 0, -1, -1, -1, -1]
+            [1, -1, 0, 0, 0, 0, 0, -1, 0],
+            [0, -1, -1, 0, 0, -1, -1, -1, -1]
         ], dtype=np.int8)
 
-
-# print(get_best_move(board, 1, 6))
-print(evaluate_board_python(board, -1))
+# print(get_valid_moves_python(board, 1))
+print(get_best_move(board, 1, 6))
+# print(evaluate_board_python(board, 1))